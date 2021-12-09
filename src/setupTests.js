9eb06cedf40dc6b7057dba096880ed3c0dea0165
avertin <alexandravertin@gmail.com>
2021-12-09 15:02:06 -0800

    Mock intersection observer to fix test

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,27 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+class MockIntersectionObserver {
+  constructor() {
+    this.root = null;
+    this.rootMargin = '';
+    this.thresholds = [];
+    this.disconnect = () => null;
+    this.observe = () => null;
+    this.takeRecords = () => null;
+    this.unobserve = () => null;
+  }
+}
+
+Object.defineProperty(window, 'IntersectionObserver', {
+  writable: true,
+  configurable: true,
+  value: MockIntersectionObserver
+});
+
+Object.defineProperty(global, 'IntersectionObserver', {
+  writable: true,
+  configurable: true,
+  value: MockIntersectionObserver
+});
