1d4ff4004089caaed1a25ded0fa34a1207eada91
Matt Brooks <mvbrooks@gmail.com>
2022-01-14 15:14:05 -0800

    Transitions on scroll for floating download button (#24)

--- src/components/FloatingButton.jsx
+++ src/components/FloatingButton.jsx
@@ -1,9 +1,12 @@
 /** @jsxImportSource @emotion/react */
-import { downloadSvg } from 'assets';
+import { useEffect, useState } from 'react';
+import { downloadSvg, downloadAltSvg } from 'assets';
 
 const text = 'Download the report';
 const url = '';
 const color = '#CC3333';
+
+const scrollThreshold = 50;
 
 const styles = {
   button: {
@@ -19,13 +22,16 @@ const styles = {
     position: 'fixed',
     bottom: 20,
     right: 0,
+    boxShadow: '1px 4px 4px #bbb',
+    transition: 'min-width .25s linear',
     '&:active': {
       opacity: '.5'
     },
     '&:hover': {
       background: '#FFFFFF',
       cursor: 'pointer',
-      color
+      boxShadow: 'none',
+      color,
     }
   },
   downloadIcon: {
@@ -37,13 +43,93 @@ const styles = {
   }
 };
 
-const FloatingButton = () => (
-  <a href={url} target="_blank" rel="noopener noreferrer">
-    <button css={styles.button} type="button">
-      <span css={styles.buttonText}>{text}</span>
-      <img css={styles.downloadIcon} src={downloadSvg} alt="Download" />
-    </button>
-  </a>
-);
+const downloadIconDefaultStyles = {
+  white: {
+    display: 'inline-block'
+  },
+  red: {
+    display: 'none'
+  }
+};
+
+const downloadIconHoverStyles = {
+  white: {
+    display: 'none'
+  },
+  red: {
+    display: 'inline-block'
+  }
+};
+
+const scrollTopStyles = {
+  button: {
+    minWidth: '200px'
+  },
+  buttonText: {
+    display: 'inline-block'
+  }
+};
+
+const scrollDownStyles = {
+  button: {
+    minWidth: '50px',
+    boxShadow: 'none'
+  },
+  buttonText: {
+    display: 'none'
+  }
+};
+
+const FloatingButton = () => {
+  const [scrollTop, setScrollTop] = useState(0);
+  const [scrollStyles, setScrollStyles] = useState(scrollTopStyles);
+  const [downloadIconToggle, setDownoloadIconToggle] = useState(downloadIconDefaultStyles);
+
+  const handleMouseOver = () => {
+    setScrollStyles(scrollTopStyles);
+    setDownoloadIconToggle(downloadIconHoverStyles);
+  };
+
+  const handleMouseLeave = () => {
+    if (scrollTop > scrollThreshold) {
+      setScrollStyles(scrollDownStyles);
+    }
+    setDownoloadIconToggle(downloadIconDefaultStyles);
+  };
+
+  useEffect(() => {
+    const onScroll = (e) => {
+      setScrollTop(e.target.documentElement.scrollTop);
+
+      if (scrollTop > scrollThreshold) {
+        setScrollStyles(scrollDownStyles);
+      } else {
+        setScrollStyles(scrollTopStyles);
+      }
+    };
+    window.addEventListener('scroll', onScroll);
+
+    return () => window.removeEventListener('scroll', onScroll);
+  });
+
+  return (
+    <a href={url} target="_blank" rel="noopener noreferrer">
+      <button
+        css={[styles.button, scrollStyles.button]}
+        type="button"
+        onMouseOver={handleMouseOver}
+        onFocus={handleMouseOver}
+        onMouseLeave={handleMouseLeave}
+        onBlur={handleMouseLeave}
+      >
+        <span css={[styles.buttonText, scrollStyles.buttonText]}>
+          {text}
+        </span>
+        <img css={[styles.downloadIcon, downloadIconToggle.white]} src={downloadSvg} alt="Download" />
+        <img css={[styles.downloadIcon, downloadIconToggle.red]} src={downloadAltSvg} alt="Download" />
+      </button>
+    </a>
+  );
+};
 
 export default FloatingButton;
